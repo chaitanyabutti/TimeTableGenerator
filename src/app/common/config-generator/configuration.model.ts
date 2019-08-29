1f7f6a7f619635be1a74ec272904f91e490b2c18
chaitanyabutti <chaitanya.butti@outlook.com>
2019-08-29 08:47:29 +0530

    data model thing

--- src/app/common/config-generator/configuration.model.ts
+++ src/app/common/config-generator/configuration.model.ts
@@ -1,29 +1,35 @@
 import { Time } from '@angular/common';
 
-export interface Subjects {
+// export interface Subjects {
+//     name: string;
+//     shortName: string;
+//     code: string;
+// }
+
+export class Subjects {
     name: string;
     shortName: string;
     code: string;
 }
 
-export interface Days {
+export class Days {
             name: string;
             shortName: string;
 }
-export interface Periods {
+export class Periods {
             name: string;
             shortName: string;
             startTime: Time;
             endTime: Time;
 }
-export interface Rooms {
+export class Rooms {
             name: string;
             shortName: string;
             capacity: number;
             building: string;
 }
 
-export interface Teachers {
+export class Teachers {
             name: string;
             shortName: string;
             maxActivitiesInARow: number;
@@ -32,14 +38,14 @@ export interface Teachers {
             dayOff2TimesAWeek: boolean;
 }
 
-export interface Classes {
+export class Classes {
             name: string;
             shortName: string;
             maxAllowedDifference: number;
             startOn1stPeriod: boolean;
             noGapsAllowed: number;
 }
-export interface Activities {
+export class Activities {
             teacher: string;
             subject: string;
             class: string;
@@ -48,7 +54,17 @@ export interface Activities {
             alternateRoom: string;
 }
 
-export interface ConfigurationTemplate {
+export class ConfigurationTemplate {
+    Days: Days[];
+    Periods: Periods[];
+    Subjects: Subjects[];
+    Rooms: Rooms[];
+    Teachers: Teachers[];
+    Classes: Classes[];
+    Activities: Activities[];
+}
+
+// export class ConfigurationTemplate {
     // constructor(    name: string,
     //                 days: {
     //         name: string;
@@ -97,51 +113,51 @@ export interface ConfigurationTemplate {
     //     ) {
 
     //     }
-        name: string;
-    days: [{
-        name: string,
-        shortName: string,
-    }];
-periods: [{
-        name: string;
-        shortName: string;
-        startTime: Time;
-        endTime: Time;
-    }];
-subjects: [{
-        name: string;
-        shortName: string;
-        code: string;
-    }];
-rooms: [{
-        name: string;
-        shortName: string;
-        capacity: number;
-        building: string;
-    }];
-teachers: [{
-        name: string;
-        shortName: string;
-        maxActivitiesInARow: number;
-        maxGapsPerCycle: number;
-        maxGapsOncePerDay: number;
-        dayOff2TimesAWeek: boolean;
-    }];
-classes: [{
-        name: string;
-        shortName: string;
-        maxAllowedDifference: number;
-        startOn1stPeriod: boolean;
-        noGapsAllowed: number;
-    }];
-activities: [{
-        teacher: string;
-        subject: string;
-        class: string;
-        activitiesPerCyle: number;
-        desiredRoom: string;
-        alternateRoom: string;
-    }];
+//         name: string;
+//     days: [{
+//         name: string,
+//         shortName: string,
+//     }];
+// periods: [{
+//         name: string;
+//         shortName: string;
+//         startTime: Time;
+//         endTime: Time;
+//     }];
+// subjects: [{
+//         name: string;
+//         shortName: string;
+//         code: string;
+//     }];
+// rooms: [{
+//         name: string;
+//         shortName: string;
+//         capacity: number;
+//         building: string;
+//     }];
+// teachers: [{
+//         name: string;
+//         shortName: string;
+//         maxActivitiesInARow: number;
+//         maxGapsPerCycle: number;
+//         maxGapsOncePerDay: number;
+//         dayOff2TimesAWeek: boolean;
+//     }];
+// classes: [{
+//         name: string;
+//         shortName: string;
+//         maxAllowedDifference: number;
+//         startOn1stPeriod: boolean;
+//         noGapsAllowed: number;
+//     }];
+// activities: [{
+//         teacher: string;
+//         subject: string;
+//         class: string;
+//         activitiesPerCyle: number;
+//         desiredRoom: string;
+//         alternateRoom: string;
+//     }];
 
 //     name: string;
 //     days: [{
@@ -188,5 +204,5 @@ activities: [{
 //         desiredRoom: string;
 //         alternateRoom: string;
 //     }
-}
+// }
 
