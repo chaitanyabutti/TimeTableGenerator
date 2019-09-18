3a5c060f429430293aea482c587f06c07ca9981b
chaitanyabutti <chaitanya.butti@outlook.com>
2019-09-18 09:30:10 +0530

    .

--- src/app/generator/generator-home/generator-home.component.ts
+++ src/app/generator/generator-home/generator-home.component.ts
@@ -16,9 +16,11 @@ export class GeneratorHomeComponent implements OnInit {
   noOfRows;
   noOfDays;
   rowHeight;
+  innerHTML;
   configurationTemplates;
   selectedConfiguration;
   workItem;
+  dataLoaded = false;
   gridStrctures = [];
 
   constructor(public dialog: MatDialog,
@@ -34,9 +36,17 @@ export class GeneratorHomeComponent implements OnInit {
     this.noOfCols = 2;
     // this.rowHeight = this.noOfCols + ':' + this.noOfRows;
     this.getConfigData();
+    this.generateInnerHtml();
   }
 
-  getConfigData() {
+  generateInnerHtml() {
+    // this.innerHTML = "<div class='box' gdColumn='auto / span; 2;'>Empty</div>< div; class='box' gdColumn ='auto / span 5' * ngFor;= 'let day of workItem.Days' ><span style;='text-align: center'> {;{day.name;}} /span> as < /div>";
+    this.innerHTML = "<div class='box' gdColumn='auto / span 2'>Empty</div>"
+    let myContainer = document.getElementById('thisthing') as HTMLElement;
+    myContainer.innerHTML = "<div class='box' gdColumn='auto / span; 2;'>Empty</div>< div; class='box' gdColumn ='auto / span 5' * ngFor;= 'let day of workItem.Days' ><span style;='text-align: center'> {;{day.name;}} /span> as < /div>";
+  }
+
+getConfigData() {
     this.db.collection('configurations').snapshotChanges().subscribe(snaps => {
       this.configurationTemplates = snaps.map(snaps => {
         return {
@@ -47,7 +57,7 @@ export class GeneratorHomeComponent implements OnInit {
     });
   }
 
-  scheduleTimeTable() {
+scheduleTimeTable() {
     this.workItem = this.configurationTemplates[0];
     this.noOfRows = this.workItem.Classes.length;
     this.noOfRows = this.workItem.Periods.length;
@@ -55,25 +65,26 @@ export class GeneratorHomeComponent implements OnInit {
     this.initializeGridStructure();
   }
 
-  initializeGridStructure() {
-    this.noOfDays.forEach((day, dayIndex) => {
-      this.noOfRows.forEach((row, rowIndex) => {
-        this.noOfCols.forEach((column, columnIndex) => {
-          // if (dayIndex === 0 && rowIndex === 0 && columnIndex === 0) {
-            this.gridStrctures[dayIndex][rowIndex][columnIndex] = new Grid();
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].metaData = false;
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].textContent = dayIndex + ',' + rowIndex + ',' + columnIndex;
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].textColor = 'black';
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].gridSpan = 1;
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].gridColor = 'purple';
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].rowPos = rowIndex;
-            this.gridStrctures[dayIndex][rowIndex][columnIndex].colPos = columnIndex;
-          // }
-          // this.gridStrctures[0]
-        });
-      });
-    });
-    console.log("Filled up gridStructure");
+initializeGridStructure() {
+    // this.noOfDays.forEach((day, dayIndex) => {
+    //   this.noOfRows.forEach((row, rowIndex) => {
+    //     this.noOfCols.forEach((column, columnIndex) => {
+    //       // if (dayIndex === 0 && rowIndex === 0 && columnIndex === 0) {
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex] = new Grid();
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].metaData = false;
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].textContent = dayIndex + ',' + rowIndex + ',' + columnIndex;
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].textColor = 'black';
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].gridSpan = 1;
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].gridColor = 'purple';
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].rowPos = rowIndex;
+    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].colPos = columnIndex;
+    //       // }
+    //       // this.gridStrctures[0]
+    //     });
+    //   });
+    // });
+    this.dataLoaded = true;
+    console.log('Filled up gridStructure');
     console.log(this.gridStrctures);
   }
 
