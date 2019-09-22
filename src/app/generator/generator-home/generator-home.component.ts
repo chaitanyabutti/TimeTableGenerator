d8900ca28fe70182ccabe8293d5cd87a5f393005
chaitanyabutti <chaitanya.butti@outlook.com>
2019-09-22 06:58:52 +0530

    .

--- src/app/generator/generator-home/generator-home.component.ts
+++ src/app/generator/generator-home/generator-home.component.ts
@@ -21,6 +21,7 @@ export class GeneratorHomeComponent implements OnInit {
   selectedConfiguration;
   workItem;
   dataLoaded = false;
+  // gridStrctures: Grid[][][] = [];
   gridStrctures = [];
 
   constructor(public dialog: MatDialog,
@@ -36,7 +37,7 @@ export class GeneratorHomeComponent implements OnInit {
     this.noOfCols = 2;
     // this.rowHeight = this.noOfCols + ':' + this.noOfRows;
     this.getConfigData();
-    this.generateInnerHtml();
+    // this.generateInnerHtml();
   }
 
   generateInnerHtml() {
@@ -47,14 +48,31 @@ export class GeneratorHomeComponent implements OnInit {
   }
 
 getConfigData() {
-    this.db.collection('configurations').snapshotChanges().subscribe(snaps => {
-      this.configurationTemplates = snaps.map(snaps => {
-        return {
-          id: snaps.payload.doc.id,
-          ...snaps.payload.doc.data()
-        };
-      });
+  // this.db.collection('configurations').get().subscribe(configs => {
+  //   this.configurationTemplates = configs.docs;
+  //   console.log("config templates are coming");
+  //   console.log(this.configurationTemplates);
+  //   // this.scheduleTimeTable();
+  // });
+  this.db.collection('configurations').snapshotChanges().subscribe(snaps => {
+    this.configurationTemplates = snaps.map(snaps => {
+      return {
+        id: snaps.payload.doc.id,
+        ...snaps.payload.doc.data()
+      };
     });
+    console.log('this.configurationTemplates');
+    console.log(this.configurationTemplates);
+    this.scheduleTimeTable();
+  });
+    // this.db.collection('configurations').snapshotChanges().subscribe(snaps => {
+    //   this.configurationTemplates = snaps.map(snaps => {
+    //     return {
+    //       id: snaps.payload.doc.id,
+    //       ...snaps.payload.doc.data()
+    //     };
+    //   });
+    // });
   }
 
 scheduleTimeTable() {
@@ -62,30 +80,74 @@ scheduleTimeTable() {
     this.noOfRows = this.workItem.Classes.length;
     this.noOfRows = this.workItem.Periods.length;
     this.noOfDays = this.workItem.Days.length;
-    this.initializeGridStructure();
+    this.dataLoaded = true;
+    this.initializeGridStructure2();
   }
 
-initializeGridStructure() {
-    // this.noOfDays.forEach((day, dayIndex) => {
-    //   this.noOfRows.forEach((row, rowIndex) => {
-    //     this.noOfCols.forEach((column, columnIndex) => {
-    //       // if (dayIndex === 0 && rowIndex === 0 && columnIndex === 0) {
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex] = new Grid();
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].metaData = false;
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].textContent = dayIndex + ',' + rowIndex + ',' + columnIndex;
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].textColor = 'black';
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].gridSpan = 1;
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].gridColor = 'purple';
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].rowPos = rowIndex;
-    //         this.gridStrctures[dayIndex][rowIndex][columnIndex].colPos = columnIndex;
-    //       // }
-    //       // this.gridStrctures[0]
-    //     });
-    //   });
-    // });
-    this.dataLoaded = true;
+  initializeGridStructure2() {
+    console.log("gridStructure");
+    console.log(this.gridStrctures);
+    // this.gridStrctures = [];
+    this.workItem.Classes.forEach((classBro, classIndex) => {
+      this.gridStrctures[classIndex] = [];
+      this.gridStrctures[classIndex][0] = [];
+      this.gridStrctures[classIndex][0][0] = new Grid(true, classBro.name, 'black', 2, 'purple', 0, 0, 0, '0');
+      this.workItem.Days.forEach((day, dayIndex) => {
+        this.gridStrctures[classIndex][dayIndex + 1] = [];
+        this.workItem.Periods.forEach((period, periodIndex) => {
+                  // tslint:disable-next-line: max-line-length
+        this.gridStrctures[classIndex][dayIndex + 1][periodIndex] = new Grid(false, period.shortName, 'black', 1, 'purple', classIndex, dayIndex + 1, periodIndex, '0');
+        });
+      });
+    });
     console.log('Filled up gridStructure');
     console.log(this.gridStrctures);
   }
+
+// initializeGridStructure() {
+//   this.workItem.Classes.forEach((className, classIndex) => {
+//     this.gridStrctures[0][0] = new Grid();
+//     this.gridStrctures[0][0].metaData = false;
+//     this.gridStrctures[0][0].textContent = 0 + ',' + 0 + ',' + 0;
+//     this.gridStrctures[0][0].textColor = 'black';
+//     this.gridStrctures[0][0].gridSpan = 1;
+//     this.gridStrctures[0][0].gridColor = 'purple';
+//     this.gridStrctures[0][0].rowPos = 0;
+//     this.gridStrctures[0][0].colPos = 0;
+//   });
+//   this.workItem.Days.forEach((day, dayIndex) => {
+//     this.workItem.Periods.forEach((period, periodIndex) => {
+//     });
+//   });
+//   this.gridStrctures[0][0][0] = new Grid();
+//   this.gridStrctures[0][0][0].metaData = false;
+//   this.gridStrctures[0][0][0].textContent = 0 + ',' + 0 + ',' + 0;
+//   this.gridStrctures[0][0][0].textColor = 'black';
+//   this.gridStrctures[0][0][0].gridSpan = 1;
+//   this.gridStrctures[0][0][0].gridColor = 'purple';
+//   this.gridStrctures[0][0][0].rowPos = 0;
+//   this.gridStrctures[0][0][0].colPos = 0;
+
+//   // this.noOfDays.forEach((day, dayIndex) => {
+//   //     this.noOfRows.forEach((row, rowIndex) => {
+//   //       this.noOfCols.forEach((column, columnIndex) => {
+//   //         // if (dayIndex === 0 && rowIndex === 0 && columnIndex === 0) {
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex] = new Grid();
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].metaData = false;
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].textContent = dayIndex + ',' + rowIndex + ',' + columnIndex;
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].textColor = 'black';
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].gridSpan = 1;
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].gridColor = 'purple';
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].rowPos = rowIndex;
+//   //           this.gridStrctures[dayIndex][rowIndex][columnIndex].colPos = columnIndex;
+//   //         // }
+//   //         // this.gridStrctures[0]
+//   //       });
+//   //     });
+//   //   });
+//   this.dataLoaded = true;
+//   console.log('Filled up gridStructure');
+//   console.log(this.gridStrctures);
+//   }
 
 }
--- src/app/generator/grid-model.ts
+++ src/app/generator/grid-model.ts
@@ -1,10 +1,23 @@
 export class Grid {
-    metaData: boolean;
-    textContent: string;
-    textColor: string;
-    gridSpan: number;
-    gridColor: string;
-    rowPos: number;
-    colPos: number;
-    dayIndex: number;
+    // metaData: boolean;
+    // textContent: string;
+    // textColor: string;
+    // gridSpan: number;
+    // gridColor: string;
+    // rowPos: number;
+    // colPos: number;
+    // dayIndex: number;
+    // gdColumn: string;
+    constructor(
+        public metaData: boolean,
+        public textContent: string,
+        public textColor: string,
+        public gridSpan: number,
+        public gridColor: string,
+        public rowPos: number,
+        public colPos: number,
+        public dayIndex: number,
+        public gdColumn: string,
+    ) {
+    }
 }
